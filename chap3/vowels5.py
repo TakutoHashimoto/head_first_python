f358662aca6bad46c363410cfe30c81871a2d173
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-07-18 07:56:53 +0900

    make vowels5.py

---
+++ chap3/vowels5.py
@@ -0,0 +1,18 @@
+# dictionaryの初期化の部分をコメントアウトしているので、このスクリプトは実行時エラーが出る
+
+vowels = ["a", "e", "i", "o", "u"]
+word = input("英単語を入力してください。母音を探します：")
+
+found = {}
+# found["a"] = 0
+# found["e"] = 0
+# found["i"] = 0
+# found["o"] = 0
+# found["u"] = 0
+
+for letter in word:
+    if letter in vowels:
+        found[letter] += 1
+
+for k, v in sorted(found.items()):
+    print(f"{k} の出現回数は {v} 回")
