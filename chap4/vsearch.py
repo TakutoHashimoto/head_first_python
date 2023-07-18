d4182eab4b6a76ea53060060ebd842e47b9b44b9
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-07-18 23:08:40 +0900

    revise vsearch.py and make vsearch2.py

--- chap4/vsearch.py
+++ chap4/vsearch.py
@@ -1,12 +1,12 @@
-def search4vowels(word):
+def search4vowels(word: str) -> set:
     """
-    英単語に含まれる母音を表示する。
+    指定された英単語に含まれる母音を返す。
     """
     vowels = set("aeiou")
-    found = vowels.intersection(set(word))
-    print(*found, sep="\n")
+
+    return vowels.intersection(set(word))
 
 
 if __name__ == "__main__":
     word = input("入力：")
-    search4vowels(word)
+    print(search4vowels(word))
---
+++ chap4/vsearch2.py
@@ -0,0 +1,16 @@
+def search4vowels(word: str) -> set:
+    """
+    指定された英単語に含まれる母音を返す。
+    """
+    vowels = set("aeiou")
+    return vowels.intersection(set(word))
+
+def search4letters(phrase: str, letters: str) -> set:
+    """
+    指定された英単語に含まれる母音を返す。
+    """
+    return set(letters).intersection(set(phrase))
+
+
+if __name__ == "__main__":
+    print(search4letters("hitch-hiker", "aeiou"))
