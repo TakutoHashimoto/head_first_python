5b8d2332e0723ec3d59ada0217b05287309239b3
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-07-22 02:36:32 +0900

    make log_request method to vsearch4web.py

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -4,6 +4,14 @@ from vsearch import search4letters
 app = Flask(__name__)
 
 
+def log_request(req: str, res: str) -> None:
+    """
+    リクエストとレスポンスをログファイルに記録する
+    """
+    with open("log/vsearch.log", "a") as log:
+        print(req, res, file=log)
+
+
 @app.route("/")
 @app.route("/entry")
 def entry_page() -> str:
@@ -19,6 +27,9 @@ def do_search() -> str:
     letters = request.form["letters"]
     title = "検索結果"
     results = str(search4letters(phrase, letters))
+
+    log_request(request, results)
+
     return render_template(
         "results.html",
         the_phrase=phrase,
@@ -26,6 +37,14 @@ def do_search() -> str:
         the_title=title,
         the_results=results
     )
+
+
+@app.route("/viewlog")
+def view_log() -> str:
+    with open("log/vsearch.log") as log:
+        contents = log.read()
+
+    return contents
 
 
 if __name__ == "__main__":
