dd66bc81b9fe8bad90494edf0b0582a03f7900a4
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-07-21 23:08:50 +0900

    update do_search method

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -9,18 +9,27 @@ def hello() -> str:
     return "Hello world from Flask!"
 
 
-@app.route("/search4", methods=["POST"])
-def do_search() -> str:
-    phrase = request.form["phrase"]
-    letters = request.form["letters"]
-    return str(search4letters(phrase, letters))
-
-
 @app.route("/entry")
 def entry_page() -> str:
     return render_template(
         "entry.html",
         the_title="Web版のsearch4lettersにようこそ!"
     )
+
+
+@app.route("/search4", methods=["POST"])
+def do_search() -> str:
+    phrase = request.form["phrase"]
+    letters = request.form["letters"]
+    title = "検索結果"
+    results = str(search4letters(phrase, letters))
+    return render_template(
+        "results.html",
+        the_phrase=phrase,
+        the_letters=letters,
+        the_title=title,
+        the_results=results
+    )
+
 
 app.run(debug=True)
